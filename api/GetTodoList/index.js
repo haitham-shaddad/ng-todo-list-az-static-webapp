2db712e9c2fea1280b24b65a74a629c646e7bb78
Haitham Shaddad <haitham.shaddad@outlook.com>
2020-06-05 09:15:43 +1000

    fix the api

--- api/GetTodoList/index.js
+++ api/GetTodoList/index.js
@@ -1,10 +1,10 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     const responseMessage =  [
-        new { Title = "Read Microfronts book", IsCompleted = false},
-        new { Title = "Post a blog about Project Tye", IsCompleted = false},
-        new { Title = "Get some exercises", IsCompleted = true},
-        new { Title = "Do more stuff", IsCompleted = false},
+         { Title = "Read Microfronts book", IsCompleted = false},
+         { Title = "Post a blog about Project Tye", IsCompleted = false},
+         { Title = "Get some exercises", IsCompleted = true},
+         { Title = "Do more stuff", IsCompleted = false},
     ];
 
     context.res = {
